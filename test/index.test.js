62d75b1eb6960d14344a7962b6dffee20ad7c72c
eliyahukoren <elik.koren@gmail.com>
2022-12-26 17:53:32 +0200

    add some tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,24 @@ describe("Test Jump Game", () => {
 	it("[2, 3, 1, 1, 4] should be true", () => {
 		assert.equal(canJump([2, 3, 1, 1, 4]), true);
 	});
+
 	it("[3, 2, 1, 0, 4] should be false", () => {
 		assert.equal(canJump([3, 2, 1, 0, 4]), false);
 	});
+
+	it("[ 0, 5, 4 ] should be false", () => {
+		assert.equal(canJump([0, 5, 4]), false);
+	});
+
+	it("[ 0, 9, 0, 9, 3 ] should be false", () => {
+		assert.equal(canJump([0, 9, 0, 9, 3]), false);
+	});
+
+	it("[7, 10, 6, 2, 5, 9, 10, 8, 2, 5, 6, 10, 0, 0, 5, 4] should return true", () => {
+		assert.equal(
+			canJump([7, 10, 6, 2, 5, 9, 10, 8, 2, 5, 6, 10, 0, 0, 5, 4]),
+			true
+		);
+	});
+
 });
